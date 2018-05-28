dacf6868a9542d004a2f06db1ecaa481c390072e
SkyNet <nme312.noreply.github.com>
2018-05-28 12:02:09 -0500

    add db setup

---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+use burgers_db;
+
+insert into burgers (burger_name, devoured)
+values ("Bacon Cheeseburger", 0);
+
+insert into burgers (burger_name, devoured)
+values ("BBQ Burger", 0);
+
+insert into burgers (burger_name, devoured)
+values ("Bison Burger", 0);

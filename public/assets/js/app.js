355cf927e2d9309f26c6c5d519b9c5ac1c4bdc05
SkyNet <nme312.noreply.github.com>
2018-05-31 23:32:13 -0500

    minor style changes

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,15 +1,5 @@
 $(document).ready(function () {
     console.log("log");
-
-    $("#devour-button").on("click", function (event) {
-        event.preventDefault();
-        var id = $(this).data("burger-id");
-        $.ajax({
-            url: "/update/" + id,
-            method: "PUT",
-        }).then(response => location.reload());
-    });
-
     $(".add-burger-button").on("click", function (event) {
         event.preventDefault();
         var data = { burger_name: $("#new-burger").val() };
@@ -19,5 +9,18 @@ $(document).ready(function () {
             method: "POST",
             data: data
         }).then(response => location.reload());
+    });
+
+    $("#devour-button").on("click", function (event) {
+        event.preventDefault();
+        var id = $(this).data("burger-id");
+        console.log(id);
+        $.ajax({
+            url: "/update/" + id,
+            method: "PUT",
+        }).then(response => {
+            location.reload()
+            console.log(id);
+        });
     });
 });
